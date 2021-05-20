e9d570837305d6e39c5499a750959667f90a4f19
shaan1232 <shaan1232@hotmail.com>
2021-05-20 19:54:01 -0400

    Changing current program working on

--- Kata.cs
+++ Kata.cs
@@ -1,75 +1,42 @@
 using System;
-using System.Linq;
-
-public class Kata
+public static class Kata
 {
-    private static Boolean isUsed(char[] usedLettersArray, char c)
+    private static int getLower(int n)
     {
-        return (usedLettersArray.Contains(c));
-    }
 
-    public static int DuplicateCount(string str)
-    {
-        // we can loop for each letter inside the input, but that will be
-        // very unoptimized, since it would run 26 times
+        // just count thr rows (3 + 2 + 1)
+        int rows = 1; // current row
+        int countSum = 0; // counts current sum
 
-        // a better solution would be to keep an array of each letter
-        // that has already been run, and skip if it has. And go left 
-        // to right on the WHOLE input.
-        char[] usedLetter = new char[36]; // this takes into account all english letters a-z and numerals 0-9
-        char C;
-        char c;
-        int numDoubles = 0;
-        int counter = 0;
-        char d;
-
-        for (int i = 0; i < str.Length; i++)
+        // each row carries 'rows' number of odd ints, so add +2 to countSum
+        while (rows < n)
         {
-            if (Char.IsUpper(str[i]))
+            for (int i = 0; i < rows; i++)
             {
-                C = str[i];
-                c = Char.ToLower(C);
-            }
-            else
-            {
-                c = str[i];
-                C = Char.ToUpper(c);
-            }
-
-            if (!isUsed(usedLetter, c))
-            {
-                usedLetter[counter] = c;
-                counter++;
-                for (int j = i + 1; j < str.Length; j++)
-                {
-                    if (Char.IsUpper(str[j]))
-                    {
-                        d = Char.ToLower(str[j]);
-                    }
-                    else
-                    {
-                        d = str[j];
-                    }
-                    if (c == d)
-                    {
-                        numDoubles++;
-                        Console.WriteLine(c + " and " + d + " is a double!");
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine(c + " and " + d + " is NOT a double!");
-
-                    }
-                }
+                countSum += 2;
             }
         }
-        return numDoubles;
+        return countSum;
+
     }
 
+    public static long rowSumOddNumbers(long n)
+    {
+        // TODO
+        // rowSumOddNumbers(4) = 3 + 2 + 1
+        //getLower(System.out.println(countSum));
+
+
+
+        // 1
+        // 2
+        // 3
+        // 4
+        // 5
+
+    }
     static void Main(String[] args)
     {
-        Console.WriteLine(DuplicateCount("Hello World"));
+        Console.WriteLine(getLower(4));
     }
-
 }
