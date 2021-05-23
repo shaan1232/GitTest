6ada3063214500486b04ff49fc4163a349aade78
shaan1232 <shaan1232@hotmail.com>
2021-05-23 19:39:12 -0400

    New Kata program.

--- Kata.cs
+++ Kata.cs
@@ -1,13 +1,49 @@
 using System;
+using System.Globalization;
+
 public class Number
 {
-    public int DigitalRoot(long n)
+    public static bool isValidSum(int n)
+    {
+        return (n < 10);
+    }
+    static int DigitalRoot(long n)
     {
         // Your awesome code here!
+        int sum = 0;
+        bool flag = false;
+        string num = n.ToString();
+        while (sum < 10 && !flag)
+        {
+            foreach (char c in num)
+            {
+                sum += CharUnicodeInfo.GetDecimalDigitValue(c);
+            }
+            if (isValidSum(sum))
+            {
+                flag = true;
+            }
+            else
+            {
+                num = sum.ToString();
+                sum = 0;
+            }
+
+        }
+        return sum;
     }
 
-    static void main(String[] args)
+    private static void testcases()
     {
+        Console.WriteLine(DigitalRoot(16) == 7);
+        Console.WriteLine(DigitalRoot(456) == 6);
+        Console.WriteLine(DigitalRoot(942) == 6);
+        Console.WriteLine(DigitalRoot(132189) == 6);
+        Console.WriteLine(DigitalRoot(493193) == 2);
 
+    }
+    static void Main(String[] args)
+    {
+        testcases();
     }
 }
