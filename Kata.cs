7aeb47312540623f9c7fb118529432e1bf98a55b
shaan1232 <shaan1232@hotmail.com>
2021-05-23 19:13:57 -0400

    new kata project

--- Kata.cs
+++ Kata.cs
@@ -1,52 +1,13 @@
 using System;
-public class Kata
+public class Number
 {
-    public static int FindEvenIndex(int[] arr)
+    public int DigitalRoot(long n)
     {
-        int right = 0;
-        int left = 0;
-        int counter = arr.Length - 1;
-        for (int i = 0; i < arr.Length; i++)
-        {
-            left += arr[i];
-        }
-        if (left == 0)
-        {
-            return 0;
-        }
-        left -= arr[arr.Length - 1];
-        for (int i = arr.Length - 1; i > 0; i--)
-        {
-            if (right != left)
-            {
-                left -= arr[i - 1];
-                right += arr[i];
-            }
-            printStatus(right, left);
-            counter--;
-            if (right == left)
-            {
-                return counter;
-            }
-        }
-        return -1;
+        // Your awesome code here!
     }
 
-    static void print(int input)
+    static void main(String[] args)
     {
-        Console.WriteLine(input);
-    }
-    static void print(String input)
-    {
-        Console.WriteLine(input);
-    }
-    static void printStatus(int right, int left)
-    {
-        print("right:" + right + " left:" + left);
-    }
-    static void Main(String[] args)
-    {
-        int[] arr = { 1, 2, 3, 4, 5, 6 };
-        print(FindEvenIndex(arr));
+
     }
 }
