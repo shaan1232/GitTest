08c7a7a65d1e302c0c7c05e22d72384562d0ab5b
shaan1232 <shaan1232@hotmail.com>
2021-05-24 00:32:50 -0400

    Found a binary search tree online that is nice to study

--- BinarySearchTree.cs
+++ BinarySearchTree.cs
@@ -1,67 +1,136 @@
-public class BinarySearchTree
+// this whole project was copied and pasted from https://www.csharpstar.com/csharp-program-to-implement-binary-search-tree-traversal/
+// I am using it to review and learn.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Collections;
+
+namespace BinarySearchTree
 {
-    public Node root { get; set; }
-    public BinarySearchTree()
-    {
-        root = null;
-    }
 
-    public void insertElement(int nodeContent)
+    public class BinarySearchTree
     {
-        Node newNode = new Node();
-        newNode.content = nodeContent;
-        // if the root is empty, this will be the start of a new BST
-        if (root == null)
+
+        public class Node
         {
-            root = newNode;
-        }
-        // if the root is not empty, a tree already exists.
-        else
-        {
-            Node current = root;
-            Node parent;
-            while (true)
+            public int Data;
+            public Node Left;
+            public Node Right;
+            public void DisplayNode()
             {
-                parent = current; // set the parent to whatever the current root is.
-                if (nodeContent < current.content)
-                {
-                    current = current.Left;
-                    if (current == null) // This is for when we reach the bottom of tree
-                    {
-                        parent.Left = newNode;
-                        break;
-                    }
-                }
-
-                else
-                {
-                    current = current.Right;
-                    if (current == null) // When we reach the bottom of tree
-                    {
-                        parent.Right = newNode;
-                        break;
-                    }
-                }
-
+                Console.Write(Data + " ");
             }
         }
-    }
+        public Node root;
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+        public void Insert(int i)
+        {
+            Node newNode = new Node();
+            newNode.Data = i;
+            if (root == null)
+                root = newNode;
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (i < current.Data)
+                    {
+                        current = current.Left;
+                        if (current == null)
+                        {
+                            parent.Left = newNode;
+                            break;
+                        }
+                    }
 
-    public void Main(String[] args)
-    {
+                    else
+                    {
+                        current = current.Right;
+                        if (current == null)
+                        {
+                            parent.Right = newNode;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
 
-        BinarySearchTree nums = new BinarySearchTree();
-        nums.Insert(50);
-        nums.Insert(17);
-        nums.Insert(23);
-        nums.Insert(12);
-        nums.Insert(19);
-        nums.Insert(54);
-        nums.Insert(9);
-        nums.Insert(14);
-        nums.Insert(67);
-        nums.Insert(76);
-        nums.Insert(72);
+        public Node ReturnRoot()
+        {
+            return root;
+        }
+
+        static void PreOrder(Node root)
+        {
+            if (root != null)
+            {
+                Console.Write(root.Data + " ");
+                PreOrder(root.Left);
+                PreOrder(root.Right);
+            }
+        }
+
+        static void PostOrder(Node root)
+        {
+            if (root != null)
+            {
+                PostOrder(root.Left);
+                PostOrder(root.Right);
+                Console.Write(root.Data + " ");
+            }
+
+
+            // if the root is null, it backtracks. Meaning that once its done going left, it will 
+            // start checking right recursively.
+        }
+
+        static void InOrder(Node root)
+        {
+            if (root != null)
+            {
+                InOrder(root.Left);
+                Console.Write(root.Data + " ");
+                InOrder(root.Right);
+            }
+
+
+        }
+
+
+        static void Main()
+        {
+            Console.WriteLine("Start");
+            BinarySearchTree nums = new BinarySearchTree();
+            nums.Insert(50);
+            nums.Insert(17);
+            nums.Insert(23);
+            nums.Insert(12);
+            nums.Insert(19);
+            nums.Insert(54);
+            nums.Insert(9);
+            nums.Insert(14);
+            nums.Insert(67);
+            nums.Insert(76);
+            nums.Insert(72);
+            Console.WriteLine("End");
+            Console.WriteLine("preorder: ");
+            PreOrder(nums.ReturnRoot());
+            Console.WriteLine("\ninorder: ");
+            InOrder(nums.ReturnRoot());
+            Console.WriteLine("\npostorder: ");
+            PostOrder(nums.ReturnRoot());
+        }
     }
 
 }
